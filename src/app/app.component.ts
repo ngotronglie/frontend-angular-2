98b2540c9eeb5a1228aade12e8f7ded4e44c0739
Ngo Trong Liem <127607692+ngotronglie@users.noreply.github.com>
2025-05-27 15:14:07 +0700

    done page shop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+import { HomeComponent} from './layouts/home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'main layout';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './layouts/home/home.component';
+import {ShopComponent} from './layouts/shop/shop.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    component: HomeComponent 
+  },
+  { 
+    path: 'shop', 
+    component: ShopComponent 
+  },
+  { 
+    path: '**', 
+    redirectTo: '' 
+  }
+];
